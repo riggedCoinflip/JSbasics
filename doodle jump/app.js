dddf444df540c114f051a7265e09a50903be03d4
riggedCoinflip <tikalker@gmail.com>
2020-10-04 00:29:12 +0200

    v2.1 bug fixes

--- doodle jump/app.js
+++ doodle jump/app.js
@@ -1,23 +1,18 @@
-//improve cam movement
-
 document.addEventListener('DOMContentLoaded', () => {
     const grid = document.querySelector('.grid');
-    const doodler = document.createElement('div');
     const gridHeight = document.querySelector('.grid').clientHeight;
+    const allKeys = ['ArrowLeft', 'ArrowRight', 'p', 'P'];
+    let paused = false;
+    let doodler;
     let doodlerLeftSpace;
     let doodlerBottomSpace;
     let isGameOver = false;
     let platforms = [];
-    let upTimerId;
-    let downTimerId;
-    let movePlatformsTimerId;
-    let controlTimerId;
-    let doodlerYAcceletation = 0;
-    let isJumping = false;
-    let tickTime = 25;
-    const allKeys = ['ArrowLeft', 'ArrowRight'];
+    let doodlerYSpeed;
+    let tickTime = 30;
+    let tickTimerId;
     let keysDown = [];
-    let score = 10;
+    let score = 0;
     let jumpSpeed = 20;
     let heightNextPlatform = 0;
 
@@ -42,7 +37,7 @@ document.addEventListener('DOMContentLoaded', () => {
             let newPlatform = new Platform(heightNextPlatform);
             grid.appendChild(newPlatform.visual);
             platforms.push(newPlatform);
-            console.log(platforms);
+            //console.log(platforms);
             heightNextPlatform += platformGap();
         }
     }
@@ -54,20 +49,101 @@ document.addEventListener('DOMContentLoaded', () => {
             (Math.exp(1) * (2 * x + 1)) / (Math.exp(1) * (2 * x + 1) + 5) + 0.1; //max is always higher than min for x > 0. Plot it
         max = max > 1 ? 1 : max; //limits maximum to 1
         const factor = Math.random() * (max - min) + min;
-        const toReturn =
-            Math.floor(((jumpSpeed * jumpSpeed + 1) / 2) * factor) - 10;
+        const toReturn = Math.floor(((jumpSpeed * jumpSpeed + 1) / 2) * factor);
         //console.log(`x: ${x} min:${min} max:${max} factor:${factor} toReturn:${toReturn}`);
         return toReturn > 15 ? toReturn : 15;
     }
 
-    function movePlatforms() {
-        if (doodlerBottomSpace > (jumpSpeed * jumpSpeed + 1) / 2 + 30) {
-            let changeBy;
-            if (doodlerBottomSpace > 450) {
-                changeBy = 15;
-            } else {
-                changeBy = 4;
+    function createDoodler() {
+        doodler = document.createElement('div');
+        grid.appendChild(doodler);
+        doodler.classList.add('doodler');
+        doodlerLeftSpace = platforms[0].left;
+        doodler.style.left = doodlerLeftSpace + 'px';
+        doodlerBottomSpace = platforms[0].bottom - 100;
+        doodler.style.bottom = doodlerBottomSpace + 'px';
+        doodlerYSpeed = 15;
+    }
+
+    function advanceTick() {
+        movementX();
+        movementY();
+        movePlatforms();
+    }
+
+    function movementX() {
+        if (keysDown.length > 0) {
+            for (const key of keysDown) {
+                if (key === 'ArrowLeft') {
+                    moveLeft();
+                    break;
+                }
+                if (key === 'ArrowRight') {
+                    moveRight();
+                    break;
+                }
             }
+        }
+        doodler.style.left = doodlerLeftSpace + 'px';
+    }
+
+    function moveLeft() {
+        doodlerLeftSpace -= 10;
+        doodlerLeftSpace = doodlerLeftSpace > 0 ? doodlerLeftSpace : 0;
+    }
+
+    function moveRight() {
+        doodlerLeftSpace += 10;
+        doodlerLeftSpace =
+            doodlerLeftSpace < 400 - 60 ? doodlerLeftSpace : 400 - 60;
+    }
+
+    /**
+     * IMPORTANT: The current method of logging which keys are held down has a fault:
+     * If you hold down a button (lets say: 'p'), while holding it down press Shift,
+     * then up the button, it will try to remove the Upper version of that key ('P').
+     * @param {*} e Pressed Key
+     */
+    function onKeyDown(e) {
+        //console.log(`Key pressed: ${e.key}`);
+        if (allKeys.includes(e.key) && !keysDown.includes(e.key)) {
+            keysDown.unshift(e.key);
+            if (['p', 'P'].includes(e.key)) {
+                clearInterval(tickTimerId);
+                if (paused) {
+                    //unpause
+                    tickTimerId = setInterval(advanceTick, tickTime);
+                }
+                paused = !paused;
+                console.log(`Game is now ${paused ? 'paused' : 'unpaused'}`);
+            }
+        }
+        //console.log(`Currently held down keys: ${keysDown}`);
+    }
+
+    function onKeyUp(e) {
+        if (allKeys.includes(e.key)) {
+            i = keysDown.indexOf(e.key);
+            if (i > -1) {
+                keysDown.splice(i, 1);
+            }
+        }
+        //console.log(`Currently held down keys: ${keysDown}`);
+    }
+
+    function movementY() {
+        doodlerBottomSpace += doodlerYSpeed;
+        doodlerYSpeed -= 1;
+        if (doodlerYSpeed < 0) {
+            checkCollision();
+            checkGameOver();
+        }
+        doodler.style.bottom = doodlerBottomSpace + 'px';
+    }
+
+    function movePlatforms() {
+        if (doodlerBottomSpace > 250) {
+            const changeBy = doodlerBottomSpace - 250;
             score += changeBy;
             heightNextPlatform -= changeBy;
             doodlerBottomSpace -= changeBy;
@@ -96,96 +172,46 @@ document.addEventListener('DOMContentLoaded', () => {
         }
     }
 
-    function createDoodler() {
-        grid.appendChild(doodler);
-        doodler.classList.add('doodler');
-        doodlerLeftSpace = platforms[0].left;
-        doodler.style.left = doodlerLeftSpace + 'px';
-        doodlerBottomSpace = platforms[0].bottom + 20;
-        doodler.style.bottom = doodlerBottomSpace + 'px';
-    }
-
-    function jump() {
-        clearInterval(downTimerId);
-        doodlerYAcceletation = jumpSpeed;
-        isJumping = true;
-        upTimerId = setInterval(function () {
-            doodlerBottomSpace += doodlerYAcceletation;
-            doodler.style.bottom = doodlerBottomSpace + 'px';
-            doodlerYAcceletation -= 1;
-            if (doodlerYAcceletation <= 0) {
-                fall();
-            }
-        }, tickTime);
-    }
-
-    function fall() {
-        clearInterval(upTimerId);
-        isJumping = false;
-        downTimerId = setInterval(function () {
-            doodlerBottomSpace += doodlerYAcceletation;
-            doodlerYAcceletation -= 1;
-            if (doodlerBottomSpace <= 0) {
-                doodler.style.bottom = '0px';
-                gameOver();
-            } else {
-                doodler.style.bottom = doodlerBottomSpace + 'px';
-                platforms.forEach((platform) => {
-                    if (
-                        //@notes1
-                        doodlerBottomSpace >=
-                            platform.bottom + 15 + doodlerYAcceletation &&
-                        doodlerBottomSpace <= platform.bottom + 15 &&
-                        doodlerLeftSpace + 60 >= platform.left &&
-                        doodlerLeftSpace <= platform.left + 85
-                    ) {
-                        //removePlatform(platform); //makes platforms disappear after 1 jump
-                        //console.log('landed!');
-                        //console.log(doodler, platform);
-                        jump();
-                    }
-                });
-            }
-        }, tickTime);
-    }
-
-    function onKeyDown(e) {
-        if (allKeys.includes(e.key) && !keysDown.includes(e.key)) {
-            keysDown.unshift(e.key);
+    function checkGameOver() {
+        if (doodlerBottomSpace <= 0) {
+            doodlerBottomSpace = 0;
+            doodler.style.bottom = '0px';
+            gameOver();
         }
     }
 
-    function onKeyUp(e) {
-        if (allKeys.includes(e.key)) {
-            i = keysDown.indexOf(e.key);
-            if (i > -1) {
-                keysDown.splice(i, 1);
+    function checkCollision() {
+        const doodlerCoords = doodlerCoordinates();
+        for (let i = platforms.length - 1; i >= 0; i--) {
+            const platform = platforms[i];
+            const collidesX1 = doodlerCoords[0][1] >= platform.left;
+            const collidesX2 = doodlerCoords[0][0] <= platform.left + 85;
+            const collidesY1 =
+                doodlerCoords[1][0] >= platform.bottom + 15 + doodlerYSpeed; //Y speed is negative
+            const collidesY2 = doodlerCoords[1][0] <= platform.bottom + 15;
+            if (collidesX1 && collidesX2 && collidesY1 && collidesY2) {
+                doodlerBottomSpace = platform.bottom + 15;
+                //removePlatform(platform); //makes platforms disappear after 1 jump
+                doodlerYSpeed = jumpSpeed;
+                break;
             }
         }
     }
 
-    function control() {
-        if (keysDown.length > 0) {
-            if (keysDown[0] === 'ArrowLeft') {
-                moveLeft();
-            }
-            if (keysDown[0] === 'ArrowRight') {
-                moveRight();
-            }
-        }
-    }
-
-    function moveLeft() {
-        if (doodlerLeftSpace >= 0) {
-            doodlerLeftSpace -= 10;
-            doodler.style.left = doodlerLeftSpace + 'px';
-        }
-    }
-
-    function moveRight() {
-        if (doodlerLeftSpace <= 400 - 60) {
-            doodlerLeftSpace += 10;
-            doodler.style.left = doodlerLeftSpace + 'px';
+    function doodlerCoordinates() {
+        if (document.querySelector('.doodler') !== null) {
+            const x1 = doodlerLeftSpace;
+            const x2 = x1 + document.querySelector('.doodler').clientWidth;
+            const y1 = doodlerBottomSpace;
+            const y2 = y1 + document.querySelector('.doodler').clientHeight;
+            return [
+                [x1, x2],
+                [y1, y2],
+            ];
+        } else {
+            console.warn(
+                'could not return coordinates because doodler doesnt exist'
+            );
         }
     }
 
@@ -194,13 +220,11 @@ document.addEventListener('DOMContentLoaded', () => {
             console.log(`FPS: ${1000 / tickTime}`);
             createPlatforms();
             createDoodler();
+
             document.addEventListener('keydown', onKeyDown);
             document.addEventListener('keyup', onKeyUp);
-            movePlatformsTimerId = setInterval(movePlatforms, tickTime);
-            controlTimerId = setInterval(control, tickTime);
-            setTimeout(() => {
-                fall();
-            }, tickTime * 6 + 300);
+
+            tickTimerId = setInterval(advanceTick, tickTime);
         }
     }
 
@@ -208,12 +232,10 @@ document.addEventListener('DOMContentLoaded', () => {
         isGameOver = true;
         console.log('Game Over!');
         grid.innerHTML = score;
-        clearInterval(movePlatformsTimerId);
-        clearInterval(controlTimerId);
-        clearInterval(upTimerId);
-        clearInterval(downTimerId);
+
+        clearInterval(tickTimerId);
     }
 
-    //attach to button
+    //TODO attach to button
     start();
 });
